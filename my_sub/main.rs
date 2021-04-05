a7668f95234b4ccc88b8ff5023fd59a9217b228e
NadHD <45095802+NadHD@users.noreply.github.com>
2021-04-05 17:42:31 +0200

    Add files via upload

--- my_sub/main.rs
+++ my_sub/main.rs
@@ -1,4 +1,3 @@
-//Importing dependencies
 use iota::client as iota_client;
 
 use iota_streams::app_channels::api::tangle::{
@@ -16,23 +15,30 @@ use anyhow::{Result, bail};
 use std::env;
 
 fn main() {
-    // Change the default settings to use a lower minimum weight magnitude for the Devnet
     let mut send_opt = SendTrytesOptions::default();
     send_opt.min_weight_magnitude = 9;
     send_opt.local_pow = false;
 
     let url = "https://nodes.devnet.iota.org:443";
 
-    // Connect to an IOTA node
     let client: Client = Client::new(send_opt, iota_client::ClientBuilder::new().node(url).unwrap().build().unwrap());
     let encoding = "utf-8";
     let mut subscriber = Subscriber::new("PROVAPROVAPROVAPROVA", encoding, PAYLOAD_BYTES, client);
 
     let args: Vec<String> = env::args().collect();
+    let mut i = 0;
+
+    for element in &args {
+        println!("TEST: Element {} = {}", i, element);
+        i = i + 1;
+    }
 
     let channel_address = &args[1];
+    println!("TEST: channel_address = {}", channel_address.to_string());
     let announce_message_identifier = &args[2];
+    println!("TEST: announce_message_identifier = {}", announce_message_identifier.to_string());
     let signed_message_identifier = &args[3];
+    println!("TEST: signed_message_identifier = {}", signed_message_identifier.to_string());
 
     match get_announcement(&mut subscriber, &channel_address.to_string(), &announce_message_identifier.to_string()){
         Ok(()) => (),
@@ -62,7 +68,9 @@ fn get_announcement<T: Transport>(subscriber: &mut Subscriber<T>, channel_addres
 
     //Printing the Announce message
     println!("Receiving announcement message");
+    println!("TEST: announcement_link = {}", announcement_link);
     subscriber.receive_announcement(&announcement_link)?;
+    println!("TEST ho ricevuto il messaggio");
 
     Ok(())
 }
